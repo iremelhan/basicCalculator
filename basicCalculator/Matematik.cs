9158b01c4c5de050e547d3c53d85206631755061
İrem Elhan <iremelhaan@gmail.com>
2023-06-26 14:32:31 +0300

    BasicCalculator
    CSharp'ta metotları kullanarak basit bir hesap makinesi tasarladım. / I designed a simple calculator using methods in CSharp.

---
+++ basicCalculator/Matematik.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace basicCalculator
+{
+	public class Matematik
+	{
+		public decimal Toplamaİslemi(decimal sayi1, decimal sayi2)
+		{
+            decimal sonuc = sayi1 + sayi2;
+			return sonuc;
+		}
+		public decimal Cikarmaİslemi(decimal sayi1, decimal sayi2)
+		{
+            decimal sonuc = sayi1 - sayi2;
+			return sonuc;
+		}
+		public decimal Bölmeİslemi(decimal sayi1, decimal sayi2)
+		{
+			decimal sonuc = sayi1 / sayi2;
+			return sonuc;
+		}
+		public decimal Carpmaİslemi(decimal sayi1, decimal sayi2)
+		{
+            decimal sonuc = sayi1 * sayi2;
+			return sonuc;
+		}
+		public void Menu()
+		{
+			Console.Clear();
+			Console.WriteLine("*** MENU ***");
+			Console.WriteLine("1 - Toplama");
+            Console.WriteLine("2 - Çıkarma");
+            Console.WriteLine("3 - Bölme");
+            Console.WriteLine("4 - Çarpma");
+			Console.WriteLine("-------------------------------");
+			Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz: ");
+        }
+		public void sonucEkranaYaz(decimal kullaniciSayi1, decimal kullaniciSayi2, decimal sonuc, string operators)
+		{
+			Console.WriteLine("{0} {1} {2} = {3}", kullaniciSayi1, operators, kullaniciSayi2, sonuc);
+		}
+	}
+}
+
---
+++ basicCalculator/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace basicCalculator;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Matematik M = new Matematik();
+        YenidenIslemYap:
+        M.Menu();
+        int kullaniciSecim = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Lütfen işlem yapmak istediğiniz 1. sayı değerini giriniz: ");
+        decimal kullaniciSayi1 = decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine("Lütfen işlem yapmak istediğiniz 2. sayı değerini giriniz: ");
+        decimal kullaniciSayi2 = decimal.Parse(Console.ReadLine());
+
+        decimal sonuc = 0;
+
+        switch (kullaniciSecim)
+        {
+            case 1: //Toplama
+                sonuc = M.Toplamaİslemi(kullaniciSayi1, kullaniciSayi2);
+                M.sonucEkranaYaz(kullaniciSayi1, kullaniciSayi2, sonuc, "+");
+                break;
+
+            case 2: //Çıkarma
+                sonuc = M.Cikarmaİslemi(kullaniciSayi1, kullaniciSayi2);
+                M.sonucEkranaYaz(kullaniciSayi1, kullaniciSayi2, sonuc, "-");
+                break;
+
+                case 3: //Bölme
+                sonuc = M.Bölmeİslemi(kullaniciSayi1, kullaniciSayi2);
+                M.sonucEkranaYaz(kullaniciSayi1, kullaniciSayi2, sonuc, "/");
+                break;
+
+                case 4: //Çarpma
+                sonuc = M.Carpmaİslemi(kullaniciSayi1, kullaniciSayi2);
+                M.sonucEkranaYaz(kullaniciSayi1, kullaniciSayi2, sonuc, "*");
+                break;
+
+            default:
+                Console.WriteLine("Belirtmiş olduğunuz değer liste içinde bulunamadı.");
+                Console.WriteLine(" Lütfen tekrar deneyiniz.");
+                System.Threading.Thread.Sleep(2000);
+                goto YenidenIslemYap;
+                break;
+        }
+        Console.WriteLine("Yeniden işlem yapmak ister misiniz? [E / H] ");
+        string secim = Console.ReadLine();
+
+        if (secim.ToUpper() == "E") 
+        {
+            goto YenidenIslemYap;
+        }
+
+    }
+
+}
+
+
+
+  
+
+
+
+
+
+
+
